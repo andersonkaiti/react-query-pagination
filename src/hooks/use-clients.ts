d5ab3120f48a441383f3ca3bdecb9fcec217a008
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-14 18:01:31 -0300

    :zap: perf(clients): prefetch next page for smoother pagination

--- src/hooks/use-clients.ts
+++ src/hooks/use-clients.ts
@@ -1,9 +1,12 @@
 import { usePagination } from '@hooks/use-pagination'
 import { getClients } from '@http/get-clients'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useEffect } from 'react'
 
 export function useClients(perPage = 20) {
   const { currentPage, getPagination } = usePagination()
+
+  const queryClient = useQueryClient()
 
   const { data: paginatedData, ...rest } = useQuery({
     queryKey: ['clients', { currentPage, perPage }],
@@ -14,6 +17,17 @@ export function useClients(perPage = 20) {
     totalItems: Number(paginatedData?.items),
     perPage,
   })
+
+  useEffect(() => {
+    if (pagination.hasNextPage) {
+      const nextPage = currentPage + 1
+
+      queryClient.prefetchQuery({
+        queryKey: ['clients', { currentPage: nextPage, perPage }],
+        queryFn: () => getClients(nextPage, perPage),
+      })
+    }
+  }, [currentPage, pagination.hasNextPage, perPage, queryClient])
 
   return {
     ...rest,
